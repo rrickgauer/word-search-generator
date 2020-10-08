04eca21688cb1026d036d7892bf438e3915adc4b
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-08 11:33:42 -0500

    setup api

--- src/api.word-search.php
+++ src/api.word-search.php
@@ -1,13 +1,47 @@
 <?php
 
+include('functions.php');
 
 
+if (isset($_GET['id'])) {
+  $listID = $_GET['id'];
+  
+  // build list of words
+  $words = getListWords($listID);
+  $wordList = [];
+  while ($word = $words->fetch(PDO::FETCH_ASSOC)) {
+    array_push($wordList, $word['word']);
+  }
+
+  // send 404 response if list id does not exist
+  if (count($wordList) == 0) {
+    echo http_response_code(404);
+    exit;
+  }
 
 
+  // get list data
+  $listData = getList($listID)->fetch(PDO::FETCH_ASSOC);
+  $list['list_id'] = $listData['id'];
+  $list['list_name'] = $listData['name'];
+  $list['word_count'] = $listData['count'];
+  $list['words'] = $wordList;
 
+  // return the data
+  echo '<pre>';
+  echo json_encode($list, JSON_PRETTY_PRINT);
+  echo '</pre>';
+  
+  exit;
+}
 
-
-
+else {
+  $lists = getLists()->fetchAll(PDO::FETCH_ASSOC);
+  echo '<pre>';
+  echo json_encode($lists, JSON_PRETTY_PRINT);
+  echo '</pre>';
+  exit;
+}
 
 
 ?>
--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,7 @@
 
 // return a pdo database object
 function dbConnect() {
-  include_once('db-info.php');
+  include('db-info.php');
 
   try {
     // connect to database
@@ -13,7 +13,7 @@ function dbConnect() {
     return $pdo;
 
   } catch(PDOexception $e) {
-      return 0;
+    return 0;
   }
 }
 
@@ -21,13 +21,93 @@ function dbConnect() {
 function getAlert($message, $alertType = 'success') {
   return "
   <div class=\"alert alert-$alertType alert-dismissible mt-5 mb-5 fade show\" role=\"alert\">
-    $message
-    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
-      <span aria-hidden=\"true\">&times;</span>
-    </button>
+  $message
+  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+  <span aria-hidden=\"true\">&times;</span>
+  </button>
   </div>";
 }
 
+
+// return the list names and ids
+function getLists() {
+  $stmt = '
+  SELECT l.id as list_id,
+         l.name as list_name,
+         (SELECT COUNT(id)
+          FROM   Words
+          WHERE  list_id = l.id) AS word_count
+  FROM   Lists l
+  GROUP  BY l.id
+  ORDER  BY l.name';
+
+  $sql = dbConnect()->prepare($stmt);
+  $sql->execute();
+
+  return $sql;
+}
+
+
+function getListWords($listID) {
+  $stmt = '
+  SELECT w.word
+  FROM   Words w
+  WHERE  w.list_id = :listID
+  ORDER  BY w.word ASC';
+
+  $sql = dbConnect()->prepare($stmt);
+
+  // filter and bind list id
+  $listID = filter_var($listID, FILTER_SANITIZE_NUMBER_INT);
+  $sql->bindParam(':listID', $listID, PDO::PARAM_INT);
+
+  $sql->execute();
+
+  return $sql;
+}
+
+
+function getListID($listName) {
+  $stmt = '
+  SELECT l.id
+  FROM   Lists l
+  WHERE  l.name = :listName
+  LIMIT  1';
+
+  $sql = dbConnect()->prepare($stmt);
+
+  // filter and bind list name
+  $listName = filter_var($listName, FILTER_SANITIZE_STRING);
+  $sql->bindParam(':listName', $listName, PDO::PARAM_STR);
+
+  $result = $sql->execute()->fetch(PDO::FETCH_ASSOC);
+
+  return $result['id'];
+}
+
+function getList($listID) {
+  $stmt = '
+  SELECT l.id,
+         l.name,
+         (SELECT COUNT(id)
+          FROM   Words
+          WHERE  list_id = l.id) AS count
+  FROM   Lists l
+  WHERE  l.id = :listID
+  GROUP  BY l.id
+  ORDER  BY l.name
+  LIMIT  1';
+
+  $sql = dbConnect()->prepare($stmt);
+
+  // filter and bind list id
+  $listID = filter_var($listID, FILTER_SANITIZE_NUMBER_INT);
+  $sql->bindParam(':listID', $listID, PDO::PARAM_INT);
+
+  $sql->execute();
+
+  return $sql;
+}
 
 
 
